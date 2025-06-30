eddf31138cd7633b2431d48c6a0fb1123e9765f1
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2025-06-30 12:08:59 +0330

    course validator and model added and writed

---
+++ src/modules/admin/courses/courseModel.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    headlines: {
+      type: mongoose.Types.ObjectId,
+    },
+    status: {
+      type: String,
+      enum: ["completing", "completed"],
+    },
+    lastupdate: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const model = new mongoose.model("courses", schema);
+
+module.exports = model
