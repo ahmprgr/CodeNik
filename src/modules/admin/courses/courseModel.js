3ce8ecd184b2cebb8dfe36c1396d1c9325ff8aa9
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2025-06-30 15:00:02 +0330

    some changes in course validator and model done

--- src/modules/admin/courses/courseModel.js
+++ src/modules/admin/courses/courseModel.js
@@ -10,6 +10,10 @@ const schema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    cover:{
+      type: String,
+     required: true,
+    } ,
     slug: {
       type: String,
       required: true,
--- src/utils/validators/courseValidator.js
+++ src/utils/validators/courseValidator.js
@@ -4,7 +4,7 @@ const courseValidationSchema = yup.object({
   name:yup.string().min(10).max(30).required(),
   description:yup.string().min(50).max(2000).required(),
   slug:yup.string().min(4).max(25).required(),
-  
+  cover:yup.string().required(),
 })
 
 module.exports = courseValidationSchema
